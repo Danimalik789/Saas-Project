1d4ac668cf0ce344358c48928393bef187dd27ad
Danyal malik <danyalmalik789@gmail.com>
2025-06-25 13:35:06 +0500

    created mongoDb database and connectd it via compass also created auth middlware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,43 @@
+import jwt from "jsonwebtoken"
+import User from "../models/User.js"
+
+const protect = async (req, res, next) => {
+  let token
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1]
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+      req.user = await User.findById(decoded.id).select("-password")
+
+      return next()
+    } catch (error) {
+      console.error(error)
+      res.status(401)
+      throw new Error("Not authorized, token failed")
+    }
+  }
+
+  if (!token) {
+    res.status(404)
+    throw new Error("Not authorized, token failed")
+  }
+  next()
+}
+
+const admin = (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
+    next()
+  } else {
+    res.status(401)
+    throw new Error("Not authorized as an admin")
+  }
+}
+
+export { protect, admin }
+// This code defines middleware functions for authentication and authorization in an Express.js application.
---
+++ backend/server.js
@@ -0,0 +1,33 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import morgan from "morgan"
+import colors from "colors"
+
+import connectDB from "./config/db.js"
+
+dotenv.config()
+
+connectDB()
+
+//Initialize express app
+const app = express()
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"))
+}
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.send("API is running....")
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
+  )
+})
