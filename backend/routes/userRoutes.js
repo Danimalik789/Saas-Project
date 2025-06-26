d4d46435cf247eeeb6471bde57cbefc9ebe7d6b3
Danyal malik <danyalmalik789@gmail.com>
2025-06-26 18:45:57 +0500

    created user registration and login system

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,10 @@
+import express from "express"
+const router = express.Router()
+import { authUser, registerUser } from "../controllers/userController.js"
+
+import { protect } from "../middleware/authMiddleware.js"
+
+router.route("/register").post(registerUser)
+router.post("/login", authUser)
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv"
 import cors from "cors"
 import morgan from "morgan"
 import colors from "colors"
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
+import userRoutes from "./routes/userRoutes.js"
 
 import connectDB from "./config/db.js"
 
@@ -22,6 +24,12 @@ app.use(express.json())
 app.get("/", (req, res) => {
   res.send("API is running....")
 })
+
+//Use Routes
+app.use("/api/users", userRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
