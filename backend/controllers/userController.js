768a20de6267428ca7c915e08f7b3eda85e5bd62
Danyal malik <danyalmalik789@gmail.com>
2025-06-26 19:01:36 +0500

    created a protected route to get user profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -43,7 +43,7 @@ const registerUser = asyncHandler(async (req, res) => {
     name,
     email,
     password,
-    role
+    role,
   })
 
   if (user) {
@@ -60,4 +60,53 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser, registerUser }
+// @desc    GET user profile
+// @route   GET /api/users/profile
+// @access  Private
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    })
+  } else {
+    res.status(404)
+    throw new Error("Invalid email or password")
+  }
+})
+
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Private
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+
+    if (req.body.password) {
+      user.password = req.body.password
+    }
+
+    const updatedUser = await user.save()
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      role: updatedUser.role,
+      token: generateToken(updatedUser._id),
+    })
+  } else {
+    res.status(404)
+    throw new Error("User not found")
+  }
+})
+
+export { authUser, registerUser, updateUserProfile, getUserProfile }
